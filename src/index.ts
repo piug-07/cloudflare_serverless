45bf79008f2ea8d14c0d2fcd1bcd746f605a3176
Piyushg_07 <guptapiyush315@gmail.com>
2024-06-08 16:26:27 +0530

    ./

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from 'hono'
 import { userRouter } from './routes/user';
-// import { bookRouter } from './routes/blog';
+import { bookRouter } from './routes/blog';
 
 export const app = new Hono<{
   Bindings: {
@@ -10,11 +12,15 @@ export const app = new Hono<{
 }>();
 
 app.route('/api/v1/user', userRouter)
-// app.route('/api/v1/book', bookRouter)
+app.route('/api/v1/book', bookRouter)
 
 
 
 app.get('/', (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
   return c.text('Hello Hono!')
 })
+
 export default app
---
+++ src/routes/blog.ts
@@ -0,0 +1,113 @@
+import { createPostInput, updatePostInput } from "@piyushg07/common";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { Hono } from "hono";
+import { verify } from "hono/jwt";
+
+export const bookRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+        JWT_SECRET: string;
+    },
+    Variables: {
+        userId: string
+    }
+}>();
+
+bookRouter.use(async (c, next) => {
+    const jwt = c.req.header('Authorization');
+    if (!jwt) {
+        c.status(401);
+        return c.json({ error: "unauthorized" });
+    }
+    const token = jwt.split(' ')[1];
+    const payload = await verify(token, c.env.JWT_SECRET);
+    if (!payload) {
+        c.status(401);
+        return c.json({ error: "unauthorized" });
+    }
+    c.set('userId', payload.id);
+    await next()
+});
+
+bookRouter.post('/', async (c) => {
+    const userId = c.get('userId');
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    const { success } = createPostInput.safeParse(body);
+    if (!success) {
+        c.status(400);
+        return c.json({ error: "invalid input" });
+    }
+    const post = await prisma.post.create({
+        data: {
+            title: body.title,
+            content: body.content,
+            authorId: userId
+        }
+    });
+    return c.json({
+        id: post.id
+    });
+})
+
+bookRouter.put('/', async (c) => {
+    const userId = c.get('userId');
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    const { success } = updatePostInput.safeParse(body);
+    if (!success) {
+        c.status(400);
+        return c.json({ error: "invalid input" });
+    }
+    prisma.post.update({
+        where: {
+            id: body.id,
+            authorId: userId
+        },
+        data: {
+            title: body.title,
+            content: body.content
+        }
+    });
+
+    return c.text('updated post');
+});
+
+bookRouter.get('/bulk', async (c) => {
+    const prisma = new PrismaClient({
+        datasources: { db: { url: c.env?.DATABASE_URL } },
+    }).$extends(withAccelerate());
+
+    try {
+        const posts = await prisma.post.findMany({});
+        return c.json(posts);
+    } catch (error) {
+        console.error('Error fetching posts:', error);
+        return c.json({ error: 'Error fetching posts' }, 500);
+    } finally {
+        await prisma.$disconnect();
+    }
+});
+
+bookRouter.get('/:id', async (c) => {
+    const id = c.req.param('id');
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const post = await prisma.post.findUnique({
+        where: {
+            id
+        }
+    });
+
+    return c.json(post);
+})
+
